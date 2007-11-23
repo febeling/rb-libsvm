90d0986cefb39f95037e70ab1dc8d9cd7a7e0769
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2007-11-23 18:47:04 +0000

    svm.h path for linux.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,5 +3,5 @@ require 'mkmf'
 $CFLAGS << " -save-temps -ggdb3 "
 
 find_header('ruby.h')
-have_header('svm.h')
+find_header('svm.h', "/usr/include/libsvm-2.0/libsvm/")
 create_makefile('libsvm_ext')
