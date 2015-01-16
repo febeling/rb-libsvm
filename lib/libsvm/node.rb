895d55628ab4228cf5550cbf7d84af8a80732fc0
Florian Ebeling <florian.ebeling@gmail.com>
2015-01-16 14:22:30 +0100

    Document Libsvm::Node

--- lib/libsvm/node.rb
+++ lib/libsvm/node.rb
@@ -1,6 +1,36 @@
 module Libsvm
+  # Represents a feature
+  #
+  # A feature has an index and a value.
+  #
+  # An array of features (Libsvm::Node instances) represents an
+  # example which can be classified, or in greater number can
+  # constitute the main part of a training set (Libsvm::Prolem).
+  #
+  # This class represents the struct
+  # svm_node[https://github.com/cjlin1/libsvm/blob/master/README#L357].
+  #
+  # @see Libsvm::Problem Libsvm::Problem, the training set class
   class Node
     class << self
+      # Create an array of features from collection or variable number
+      # of float values.
+      #
+      # Indices are converted to Integer, values are converted to
+      # Float. When passed a variable number of Float values, the
+      # index is implied by the arguments position. Collection
+      # arguments afford sparse feature arrays.
+      #
+      # @overload features(0.3, 0.7, 0.8, ...)
+      # @param array an array of values, or
+      #
+      # @overload features([[0, 0.3], [1, 0.7], [2, 0.8], ...])
+      # @param array an array of [index, value] tuples, or
+      #
+      # @overload features(0 => 0.3, 1 => 0.7, 2 => 0.8, 99 => 0.1)
+      # @param hash a hash from index to value
+      #
+      # @return [[Libsvm::Node]] example, i.e. an array of features
       def features(*vargs)
         array_of_nodes = []
         if vargs.size == 1
@@ -23,16 +53,35 @@ module Libsvm
         array_of_nodes
       end
 
+      # Create a feature node.
+      #
+      #   Libsvm::Node[0, 1.1] => #<Libsvm::Node:0x007fa58bb841c8>
+      #
+      # @return (Libsvm::Node)
       def [](index, value)
         new(index, value)
       end
     end
 
+    # @!attribute index
+    #   The index identifies the feature for which this node represents a value
+
+    # @!attribute value
+    #   The value of this feature in this instance
+
+    # Create a new feature node.
+    #
+    #   Libsvm::Node.new(0, 1.1) => #<Libsvm::Node:0x007fa58bb84180>
     def initialize(index=0, value=0.0)
       self.index = index
       self.value = value
     end
 
+    # Compare features for equality.
+    #
+    # Nodes with equal index and value are equal.
+    #
+    # @return [Boolean]
     def ==(other)
       index == other.index && value == other.value
     end
