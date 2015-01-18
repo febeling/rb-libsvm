66f308adf50337c2a2be0feedd6d6bdefc7d33e8
Florian Ebeling <florian.ebeling@gmail.com>
2015-01-18 19:29:25 +0100

    Work around instable order in hash in ruby 1.8

--- spec/core_ext_spec.rb
+++ spec/core_ext_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe "Create examples" do
   describe "from a hash" do
-    example = {11 => 0.11, 21 => 0.21, 101 => 0.99 }.to_example
-    example.should == Node.features({11 => 0.11, 21 => 0.21, 101 => 0.99 })
+    example = { 11 => 0.11, 21 => 0.21, 101 => 0.99 }.to_example.sort_by(&:index)
+    example.should == Node.features({11 => 0.11, 21 => 0.21, 101 => 0.99 }).sort_by(&:index)
   end
 
   describe "from an array of tuples" do
-    it "can create example from array" do
+    it "can create example from array of pairs" do
       example = [ [11, 0.11], [21, 0.21], [101, 0.99] ].to_example
-      example.should == Node.features({11 => 0.11, 21 => 0.21, 101 => 0.99 })
+      example.should == Node.features({11 => 0.11, 21 => 0.21, 101 => 0.99 }).sort_by(&:index)
     end
   end
 end
