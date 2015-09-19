2d258df54c0bde1e2651438701986cde088fc18b
Florian Ebeling <florian.ebeling@gmail.com>
2015-09-19 11:38:44 +0200

    Extract from main module
    The 'libsvm' module doesn't contain code anymore.

---
+++ lib/libsvm/svm_type.rb
@@ -0,0 +1,11 @@
+module Libsvm
+
+  # Module with constants for values that allowed for
+  # {Libsvm::SvmParameter#svm_type}. The value controls which kind of
+  # model is being trained.
+  #
+  # See the relevant section in the LIBSVM
+  # README[https://github.com/cjlin1/libsvm/blob/master/README#389]
+  # for more information.
+  module SvmType; end
+end
